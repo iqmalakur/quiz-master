9c4ea5732420282fa3dda98ce3967841fd89f83a
iqmalakur <iakbarkurnia@gmail.com>
2023-05-28 16:15:48 +0700

    test case

--- test/app/models/QuizTest.java
+++ test/app/models/QuizTest.java
@@ -4,60 +4,165 @@
  */
 package app.models;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.LinkedList;
-import org.bson.Document;
-import org.bson.types.ObjectId;
+import org.json.JSONObject;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  *
  * @author iakba
  */
 public class QuizTest {
-  private final Quiz quiz;
+  private final Quiz model;
   
-  public QuizTest(){
-    quiz = new Quiz();
+  public QuizTest() {
+    model = new Quiz();
   }
   
+  @Test
+  public void testInsert(){
+    JSONObject newData = new JSONObject("""
+                                        {
+                                          "name": "Percobaan Quiz",
+                                          "questions": ["QST001", "QST002"],
+                                          "respondents": ["RES001"],
+                                          "code": "AAAAAA",
+                                          "optField": "Profesi"
+                                        }
+                                        """);
+    assertTrue(model.insert(newData));
+    
+    LinkedList<JSONObject> data = model.get();
+    JSONObject lastData = data.getLast();
+    
+    assertEquals("Percobaan Quiz", lastData.getString("name"));
+    assertEquals("Profesi", lastData.getString("optField"));
+  }
+
   @Test
   public void testGet() {
-    LinkedList<Document> quizzes = quiz.get();
+    LinkedList<JSONObject> data = model.get();
+    JSONObject lastData = data.getLast();
     
-    ObjectId expectedId = quizzes.get(0).getObjectId("_id");
-    ObjectId actualId = new ObjectId("6465d43c7399c00748b823d6");
-    
-    Document quizById = quiz.get(expectedId);
-    
-    assertEquals(quizzes.get(0), quizById);
-    assertEquals(expectedId, actualId);
-    
-//    assertEquals()
+    assertEquals("Percobaan Quiz", lastData.getString("name"));
+    assertEquals("Profesi", lastData.getString("optField"));
   }
   
   @Test
-  public void testCrud(){
-//    m.insert(
-//      new Document()
-//        .append("name", "Test")
-//        .append("questions", questions)
-//        .append("respondents", respondents)
-//        .append("code", "ABCDE")
-//        .append("optField", "NIM")
-//    );
+  public void testGetByKeyValue() {
+    JSONObject dataByQuizCode = model.get("code", "AAAAAA");
     
-//    System.out.println(m.update(
-//      new Document()
-//        .append("name", "Test Lagi")
-//        .append("questions", questions)
-//        .append("respondents", respondents)
-//        .append("code", "EDCBA")
-//        .append("optField", "NID"),
-//      d.get(1).getObjectId("_id")
-//    ));
+    assertEquals("Percobaan Quiz", dataByQuizCode.getString("name"));
+    assertEquals("Profesi", dataByQuizCode.getString("optField"));
+    
+    JSONObject faultData = model.get("code", "ZZZ");
+    assertNull(faultData);
+  }
+  
+  @Test
+  public void testGetByQuery() {
+    JSONObject query = new JSONObject("""
+                                      {
+                                        "name": "Percobaan Quiz",
+                                        "code": "AAAAAA"
+                                      }
+                                      """);
+    JSONObject data = model.get(query);
+    
+    assertEquals("Percobaan Quiz", data.getString("name"));
+    assertEquals("Profesi", data.getString("optField"));
+    
+    JSONObject faultQuery = new JSONObject("""
+                                      {
+                                        "name": "Percobaan Quiz",
+                                        "code": "ZZZ"
+                                      }
+                                      """);
+    JSONObject faultData = model.get(faultQuery);
+    assertNull(faultData);
+  }  
+  
+  @Test
+  public void testGetById(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Quiz");
+      
+      JSONObject data = model.get(String.format("QUZ%03d", id));
+    
+      assertEquals("Percobaan Quiz", data.getString("name"));
+      assertEquals("Profesi", data.getString("optField"));
+
+      JSONObject faultData = model.get("QUZ000");
+      assertNull(faultData);
+    } catch(IOException e){
+      System.out.println(e);
+    }
+  }
+  
+  @Test
+  public void testUpdate(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Quiz");
+      
+      JSONObject beforeUpdate = model.get(String.format("QUZ%03d", id));
+      assertEquals("Percobaan Quiz", beforeUpdate.getString("name"));
+      assertEquals("Profesi", beforeUpdate.getString("optField"));
+      
+      assertTrue(model.update(
+              new JSONObject("""
+                             {
+                                "name": "Ini Percobaan Quiz",
+                                "optField": "NIM"
+                             }
+                             """),
+              String.format("QUZ%03d", id))
+      );
+      
+      JSONObject afterUpdate = model.get(String.format("QUZ%03d", id));
+      assertEquals("Ini Percobaan Quiz", afterUpdate.getString("name"));
+      assertEquals("NIM", afterUpdate.getString("optField"));
+    } catch(IOException e){
+      System.out.println(e);
+    }
+  }
+  
+  @Test
+  public void testDelete(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Quiz");
+      
+      assertNotNull(model.get(String.format("QUZ%03d", id)));
+      assertTrue(model.delete(String.format("QUZ%03d", id)));
+      assertNull(model.get(String.format("QUZ%03d", id)));
+      
+      try (OutputStream dataWrite = new FileOutputStream("data/IdCount.json")){
+        idFile.put("Quiz", idFile.getInt("Quiz") - 1);
+        byte[] byteData = idFile.toString().getBytes();
+        dataWrite.write(byteData);
+      } catch(IOException e){
+        System.out.println(e);
+      }
+    } catch(IOException e){
+      System.out.println(e);
+    }
   }
 }
 
--- test/app/models/RespondentTest.java
+++ test/app/models/RespondentTest.java
@@ -4,24 +4,162 @@
  */
 package app.models;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.LinkedList;
+import org.json.JSONObject;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
  * @author iakba
  */
 public class RespondentTest {
+  private final Respondent model;
   
   public RespondentTest() {
+    model = new Respondent();
+  }
+  
+  @Test
+  public void testInsert(){
+    JSONObject newData = new JSONObject("""
+                                        {
+                                          "name": "Otong",
+                                          "optFieldValue": "Mahasiswa"
+                                        }
+                                        """);
+    assertTrue(model.insert(newData));
+    
+    LinkedList<JSONObject> data = model.get();
+    JSONObject lastData = data.getLast();
+    
+    assertEquals("Otong", lastData.getString("name"));
+    assertEquals("Mahasiswa", lastData.getString("optFieldValue"));
   }
 
   @Test
   public void testGet() {
+    LinkedList<JSONObject> data = model.get();
+    JSONObject lastData = data.getLast();
+    
+    assertEquals("Otong", lastData.getString("name"));
+    assertEquals("Mahasiswa", lastData.getString("optFieldValue"));
   }
   
   @Test
-  public void testCrud() {
+  public void testGetByKeyValue() {
+    JSONObject dataByName = model.get("name", "Otong");
+    
+    assertEquals("Otong", dataByName.getString("name"));
+    assertEquals("Mahasiswa", dataByName.getString("optFieldValue"));
+    
+    JSONObject faultData = model.get("name", "oto ng");
+    assertNull(faultData);
   }
   
+  @Test
+  public void testGetByQuery() {
+    JSONObject query = new JSONObject("""
+                                      {
+                                        "name": "Otong",
+                                        "optFieldValue": "Mahasiswa"
+                                      }
+                                      """);
+    JSONObject data = model.get(query);
+    
+    assertEquals("Otong", data.getString("name"));
+    assertEquals("Mahasiswa", data.getString("optFieldValue"));
+    
+    JSONObject faultQuery = new JSONObject("""
+                                      {
+                                        "name": "Otong",
+                                        "optFieldValue": "Dosen"
+                                      }
+                                      """);
+    JSONObject faultData = model.get(faultQuery);
+    assertNull(faultData);
+  }  
+  
+  @Test
+  public void testGetById(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Respondent");
+      
+      JSONObject data = model.get(String.format("RES%03d", id));
+    
+      assertEquals("Otong", data.getString("name"));
+      assertEquals("Mahasiswa", data.getString("optFieldValue"));
+
+      JSONObject faultData = model.get("RES000");
+      assertNull(faultData);
+    } catch(IOException e){
+      System.out.println(e);
+    }
+  }
+  
+  @Test
+  public void testUpdate(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Respondent");
+      
+      JSONObject beforeUpdate = model.get(String.format("RES%03d", id));
+      assertEquals("Otong", beforeUpdate.getString("name"));
+      assertEquals("Mahasiswa", beforeUpdate.getString("optFieldValue"));
+      
+      assertTrue(model.update(
+              new JSONObject("""
+                             {
+                               "name": "Otong Surotong",
+                               "optFieldValue": "Dosen"
+                             }
+                             """),
+              String.format("RES%03d", id))
+      );
+      
+      JSONObject afterUpdate = model.get(String.format("RES%03d", id));
+      assertEquals("Otong Surotong", afterUpdate.getString("name"));
+      assertEquals("Dosen", afterUpdate.getString("optFieldValue"));
+    } catch(IOException e){
+      System.out.println(e);
+    }
+  }
+  
+  @Test
+  public void testDelete(){
+    try(InputStream dataRead = new FileInputStream("data/IdCount.json")) {
+      byte[] rawData = dataRead.readAllBytes();
+      
+      JSONObject idFile = new JSONObject(new String(rawData));
+      int id = idFile.getInt("Respondent");
+      
+      assertNotNull(model.get(String.format("RES%03d", id)));
+      assertTrue(model.delete(String.format("RES%03d", id)));
+      assertNull(model.get(String.format("RES%03d", id)));
+      
+      try (OutputStream dataWrite = new FileOutputStream("data/IdCount.json")){
+        idFile.put("Respondent", idFile.getInt("Respondent") - 1);
+        byte[] byteData = idFile.toString().getBytes();
+        dataWrite.write(byteData);
+      } catch(IOException e){
+        System.out.println(e);
+      }
+    } catch(IOException e){
+      System.out.println(e);
+    }
+  }
 }
